ef7b5159be43eef28740675a3ef2fcdfb7dfe5c6
nztqa <nztqa@users.noreply.github.com>
2017-11-13 13:46:31 +0900

    Remove sample data

--- imports/ui/layouts/AppLayout.js
+++ imports/ui/layouts/AppLayout.js
@@ -3,15 +3,6 @@ import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import MemoList from '../components/MemoList';
 
-// sample data
-const memos = [
-  {_id: 'memo1', content: 'This is sample data 1'},
-  {_id: 'memo2', content: 'This is sample data 2'},
-  {_id: 'memo3', content: 'This is sample data 3'},
-  {_id: 'memo4', content: 'This is sample data 4'},
-  {_id: 'memo5', content: 'This is sample data 5'},
-];
-
 export default class AppLayout extends React.Component {
   render() {
     const { memos } = this.props;
